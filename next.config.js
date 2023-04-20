eb92f9ca7cbb7d72a50d6fae35a3f4ffa748b963
VuKiet <justinbk1412@gmail.com>
2023-04-20 16:14:23 +0700

    portfolio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  assetPrefix: isProd ? 'vubakiet' : '',
+  images: {
+    unoptimized: true,
   },
 }
 
